12e3b890cf068fb5ffd69e7a8342e274dac6cdad
devtengry <imamogluomer@outlook.com>
2025-03-04 17:49:47 +0300

    auth layer started

---
+++ lib/core/network/network_service.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+
+class NetworkService {
+  late final Dio _dio;
+
+  NetworkService() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: "https://assign-api.piton.com.tr/api/rest/",
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      ),
+    );
+  }
+  Future<Response> getRequest(String endpoint) async {
+    try {
+      return await _dio.get(endpoint);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Response> postRequest(
+      String enpoint, Map<String, dynamic> data) async {
+    try {
+      return await _dio.post(enpoint, data: data);
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
