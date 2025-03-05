f888cc6804962b7819b345259cb7290be86d5ab7
devtengry <imamogluomer@outlook.com>
2025-03-05 09:24:57 +0300

    auth_repository.dart file created

--- lib/features/auth/data/repository/auth_repository.dart
+++ lib/features/auth/data/repository/auth_repository.dart
@@ -1,1 +1,23 @@
-//iki kodları ekleyelim
+import 'package:dio/dio.dart';
+import 'package:product_catalog_project/core/network/network_service.dart';
+
+class AuthRepository {
+  final NetworkService _networkService;
+
+  AuthRepository(this._networkService);
+
+  Future<Response> login(String email, String password) async {
+    return await _networkService.postRequest('/auth/login', {
+      'email': email,
+      'password': password,
+    });
+  }
+
+  Future<Response> register(String email, String password, String name) async {
+    return await _networkService.postRequest('/auth/register', {
+      'name': name,
+      'email': email,
+      'password': password,
+    });
+  }
+}
