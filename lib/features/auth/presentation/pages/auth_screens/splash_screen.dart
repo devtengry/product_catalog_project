edea7be7e2a7cb65f0c8260c22e0973221a7280b
devtengry <imamogluomer@outlook.com>
2025-03-23 14:53:39 +0300

    refactor: convert SplashScreen to ConsumerWidget and implement rotation and timer state management

--- lib/features/auth/presentation/pages/auth_screens/splash_screen.dart
+++ lib/features/auth/presentation/pages/auth_screens/splash_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:product_catalog_project/core/localizations/text_constants.dart';
 import 'package:product_catalog_project/core/theme/colors/project_colors.dart';
@@ -8,21 +12,33 @@ import 'package:product_catalog_project/features/auth/presentation/widgets/skip_
 import 'package:product_catalog_project/router/app_router.dart';
 
 @RoutePage()
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends ConsumerWidget {
   static const page = SplashRoute.new;
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final rotation = ref.watch(rotationProvider);
+    final isTimerDone = ref.watch(splashTimerProvider);
+
+    if (isTimerDone) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        context.router.replace(const LoginRoute());
+      });
+    }
+
     return Scaffold(
       backgroundColor: ProjectColors.purpleBackground,
       body: Stack(
         children: [
           Center(
-            child: SizedBox(
-              width: 200.w,
-              height: 130.h,
-              child: Image.asset('assets/images/logo.png'),
+            child: Transform.rotate(
+              angle: rotation,
+              child: SizedBox(
+                width: 200.w,
+                height: 130.h,
+                child: Image.asset('assets/images/logo.png'),
+              ),
             ),
           ),
           Positioned(
@@ -36,14 +52,12 @@ class SplashScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   AuthElevatedButton(
-                    onPressed: () => router.push(LoginRoute()),
+                    onPressed: () => context.router.replace(const LoginRoute()),
                     buttonText: TextConstants.authButtonTextLoginText,
                   ),
                   const SizedBox(height: 10),
                   SkipTextButton(
-                    onPressed: () {
-                      router.push(LoginRoute());
-                    },
+                    onPressed: () => context.router.replace(const LoginRoute()),
                     skipTextButton: TextConstants.skipText,
                   ),
                 ],
@@ -55,3 +69,57 @@ class SplashScreen extends StatelessWidget {
     );
   }
 }
+
+class SplashTimerNotifier extends StateNotifier<bool> {
+  Timer? _timer;
+
+  SplashTimerNotifier() : super(false) {
+    _timer = Timer(const Duration(seconds: 3), () => state = true);
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+}
+
+final splashTimerProvider =
+    StateNotifierProvider.autoDispose<SplashTimerNotifier, bool>(
+  (ref) => SplashTimerNotifier(),
+);
+
+class RotationNotifier extends StateNotifier<double> {
+  late Timer _timer;
+  final Stopwatch _stopwatch = Stopwatch();
+
+  RotationNotifier() : super(0) {
+    _stopwatch.start();
+    _timer = Timer.periodic(const Duration(milliseconds: 16), (_) {
+      final elapsed = _stopwatch.elapsedMilliseconds;
+
+      // 1.5 saniyelik döngü (1s dönüş + 0.5s bekleme)
+      final cyclePosition = elapsed % 1500;
+
+      if (cyclePosition < 1000) {
+        // 1 saniyelik dönüş animasyonu
+        state = (cyclePosition / 1000) * 2 * pi;
+      } else {
+        // 0.5 saniye sabit açıda kal
+        state = 2 * pi;
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _stopwatch.stop();
+    super.dispose();
+  }
+}
+
+final rotationProvider =
+    StateNotifierProvider.autoDispose<RotationNotifier, double>(
+  (ref) => RotationNotifier(),
+);
