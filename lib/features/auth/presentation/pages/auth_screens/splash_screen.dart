96dd8f88fbd09df6bdf003baede0a365ababf815
devtengry <imamogluomer@outlook.com>
2025-03-26 17:59:50 +0300

    feat: enhance SplashScreen with login and skip buttons for improved user navigation

--- lib/features/auth/presentation/pages/auth_screens/splash_screen.dart
+++ lib/features/auth/presentation/pages/auth_screens/splash_screen.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 import 'dart:math';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:product_catalog_project/core/localizations/text_constants.dart';
 import 'package:product_catalog_project/core/theme/colors/project_colors.dart';
-import 'package:product_catalog_project/features/auth/presentation/provider/auth_provider.dart';
+import 'package:product_catalog_project/features/auth/presentation/widgets/auth_elevated_button.dart';
+import 'package:product_catalog_project/features/auth/presentation/widgets/skip_text_button.dart';
 import 'package:product_catalog_project/router/app_router.dart';
 
 @RoutePage()
@@ -19,15 +22,8 @@ class SplashScreen extends ConsumerWidget {
     final isTimerDone = ref.watch(splashTimerProvider);
 
     if (isTimerDone) {
-      WidgetsBinding.instance.addPostFrameCallback((_) async {
-        final isAuthenticated =
-            await ref.read(authNotifierProvider.notifier).checkSession();
-
-        if (isAuthenticated) {
-          context.router.replace(const HomeRoute());
-        } else {
-          context.router.replace(const LoginRoute());
-        }
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        context.router.replace(const LoginRoute());
       });
     }
 
@@ -42,6 +38,29 @@ class SplashScreen extends ConsumerWidget {
                 width: 200.w,
                 height: 130.h,
                 child: Image.asset('assets/images/logo.png'),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 30.h,
+            left: 20.w,
+            right: 20.w,
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  AuthElevatedButton(
+                    onPressed: () => context.router.replace(const LoginRoute()),
+                    buttonText: TextConstants.authButtonTextLoginText,
+                  ),
+                  const SizedBox(height: 10),
+                  SkipTextButton(
+                    onPressed: () => context.router.replace(const LoginRoute()),
+                    skipTextButton: TextConstants.skipText,
+                  ),
+                ],
               ),
             ),
           ),
@@ -78,6 +97,7 @@ class RotationNotifier extends StateNotifier<double> {
     _stopwatch.start();
     _timer = Timer.periodic(const Duration(milliseconds: 16), (_) {
       final elapsed = _stopwatch.elapsedMilliseconds;
+
       final cyclePosition = elapsed % 1500;
 
       if (cyclePosition < 1000) {
--- lib/features/auth/presentation/provider/auth_provider.dart
+++ lib/features/auth/presentation/provider/auth_provider.dart
@@ -1,43 +1,8 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:product_catalog_project/core/network/network_service.dart';
+import 'package:product_catalog_project/features/auth/data/services/auth_service.dart';
 import 'package:product_catalog_project/features/auth/repos/auth_repository.dart';
 import 'package:riverpod/riverpod.dart';
+import 'package:product_catalog_project/core/network/network_service.dart';
 import '../states/auth_state.dart';
-
-class AuthService extends StateNotifier<AuthState> {
-  final AuthRepository authRepository;
-  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
-
-  AuthService(this.authRepository) : super(AuthState.initial());
-
-  Future<bool> checkSession() async {
-    final token = await _secureStorage.read(key: "auth_token");
-    if (token != null && token.isNotEmpty) {
-      state = state.copyWith(isAuthenticated: true);
-      return true;
-    } else {
-      state = state.copyWith(isAuthenticated: false);
-      return false;
-    }
-  }
-
-  Future<void> login(String email, String password) async {
-    final token = await authRepository.login(email, password);
-    await _secureStorage.write(key: "auth_token", value: token);
-    state = state.copyWith(isAuthenticated: true);
-  }
-
-  Future<void> register(String email, String password) async {
-    final token = await authRepository.register(email, password);
-    await _secureStorage.write(key: "auth_token", value: token);
-    state = state.copyWith(isAuthenticated: true);
-  }
-
-  Future<void> logout() async {
-    await _secureStorage.delete(key: "auth_token");
-    state = state.copyWith(isAuthenticated: false);
-  }
-}
 
 final authNotifierProvider = StateNotifierProvider<AuthService, AuthState>(
   (ref) {
